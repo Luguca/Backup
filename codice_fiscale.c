e9520f5ede58348f2962ccf6727352d1c912c9dd
Luguca <147193951+Luguca@users.noreply.github.com>
2024-04-05 13:43:16 +0200

    Update codice_fiscale.c

--- codice_fiscale.c
+++ codice_fiscale.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#define CODICI_FISCALI ".\\documenti\\codici_fiscali.txt"
-#define DATE ".\\documenti\\data.txt"
-#define LUOGHI "luoghi.txt"
+//#define CODICI_FISCALI ".\\documenti\\codici_fiscali.txt"
+//#define DATE ".\\documenti\\data.txt"
+//#define LUOGHI "luoghi.txt"
 char codice[16];
 int c = 0;
 void NOME();
@@ -15,14 +15,14 @@ void CONTROLLO();
 
 int main()
 {
-    FILE *fcheck;
+    /*FILE *fcheck;
     fcheck=fopen(CODICI_FISCALI, "r");
     if(fcheck==NULL)
     {
         fclose(fcheck);
         fcheck=fopen(CODICI_FISCALI,"w");
     }
-    fclose(fcheck);
+    fclose(fcheck);*/
     COGNOME();
     NOME();
     DATA();
@@ -31,9 +31,9 @@ int main()
 void COGNOME()
 {
     char cognome[20];
-    printf("inserire cognome :");
+    printf("inserire cognome: ");
     scanf("%s", cognome);
-    toupper(cognome);
+    //toupper(cognome);
     int lenn=strlen(cognome);
     for (int i = 0; i < lenn; i++)
     {
@@ -111,9 +111,9 @@ void COGNOME()
 void NOME()
 {
     char nome[20];
-    printf("inserire nome :");
+    printf("inserire nome: ");
     scanf("%s", nome);
-    toupper(nome);
+    //toupper(nome);
     int lenn=strlen(nome);
     for (int i = 0; i < lenn; i++)
     {
@@ -184,34 +184,99 @@ void NOME()
             }
         }
     }
-    printf("%s", codice);
+    printf("%s\n", codice);
     return;
 }
 
 void DATA()
 {
-    /*char fn[50];
-    char str[] = "Oggi è primavera!\n";
-    fptr = fopen("fputc_test.txt", "w");
-    for (i = 0; str[i] != '\n'; i++)
+    char mese[4];
+    printf("inserire anno: ");
+    scanf("%s", mese);
+    for (int i = 2; i < 4; i++)
     {
-        fputc(str[i], fptr);
+        codice[c]=mese[i];
+        c++;
     }
-    fclose(fptr);*/
-    FILE *fdata;
-    char date[95];
-    char data[12];
-    fdata=fopen(DATE, "r");
-    for (int i = 0; date[i] != EOF; i++)
+    printf("%s\n", codice);
+    int data;
+    while (c!=9)
     {
-        fputc(date[i], fdata);
-    }
-    
-    for (int i = 0; i < 95; i++)
+        printf("inserire mese: ");
+        scanf("%d", &data);
+        switch (data)
     {
-        if()
+    case 1:
+        codice[c]='a';
+        c = c + 1;
+        break;
+    case 2:
+        codice[c]='b';
+        c = c + 1;
+        break;
+    case 3:
+        codice[c]='c';
+        c = c + 1;
+        break;
+    case 4:
+        codice[c]='d';
+        c = c + 1;
+        break;
+    case 5:
+        codice[c]='e';
+        c = c + 1;
+        break;
+    case 6:
+        codice[c]='h';
+        c = c + 1;
+        break;
+    case 7:
+        codice[c]='l';
+        c = c + 1;
+        break;
+    case 8:
+        codice[c]='m';
+        c = c + 1;
+        break;
+    case 9:
+        codice[c]='p';
+        c = c + 1;
+        break;
+    case 10:
+        codice[c]='r';
+        c = c + 1;
+        break;
+    case 11:
+        codice[c]='s';
+        c = c + 1;
+        break;
+    case 12:
+        codice[c]='t';
+        c = c + 1;
+        break;
+    default:
+    printf("numero sbagliato\n");
+        break;
     }
-    
-
-    fclose(DATE);
+    }
+    printf("%s\n", codice);
+    char giorno[2];
+    printf("inserire giorno: ");
+    scanf("%s", giorno);
+    if(giorno[2]=='\n')
+    {
+        codice[c]=0;
+        c = c + 1;
+        codice[c]=giorno[1];
+        c = c + 1;
+    }
+    else
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            codice[c]=giorno[i];
+            c = c + 1;
+        }
+    }
+    printf("%s\n", codice);
 }
